345100efb11a74f9922878b598b4b0324159bd3d
duanshuaixing <1935829085@qq.com>
2018-09-07 16:06:34 +0800

    install-nexus.sh
    install nexus

--- install-nexus.sh
+++ install-nexus.sh
@@ -1,39 +1,42 @@
 #!/usr/bin/env bash
-
-install_nexus(){
+install_jdk(){
 
 	if [ $UID -ne 0 ];then
                 echo "Permission denied"
                 exit 1
         fi
-	
-	# install jdk
 
-	java -version
+        # install jdk
 
-	if [ $? -ne 0 ];then
-		wget http://developer1.bj.bcebos.com/linux/java/jdk-8u181-linux-x64.tar.gz 
-		wait
-		ls jdk-8u181-linux-x64.tar.gz &>/dev/null
-		
-		if [ $? -ne 0 ];then
-			echo "jdk download is filed, please retry"
-			exit 2
-		fi
+        java -version
 
-		tar -xf jdk-8u181-linux-x64.tar.gz -C /usr/local/
-		ln -s /usr/local/jdk1.8.0_181 /usr/local/java
-		 
-		echo "JAVA_HOME=/usr/local/java" >>/etc/profile
-           	echo "PATH=\$JAVA_HOME/bin:\$PATH" >>/etc/profile
-           	echo "export JAVA_HOME PATH" >>/etc/profile
-		
-		source /etc/profile
+        if [ $? -ne 0 ];then
+                wget http://developer1.bj.bcebos.com/linux/java/jdk-8u181-linux-x64.tar.gz
+                wait
+                ls jdk-8u181-linux-x64.tar.gz &>/dev/null
 
-	else
+                if [ $? -ne 0 ];then
+                        echo "jdk download is filed, please retry"
+                        exit 2
+                fi
+
+                tar -xf jdk-8u181-linux-x64.tar.gz -C /usr/local/
+                ln -s /usr/local/jdk1.8.0_181 /usr/local/java
+
+                echo "JAVA_HOME=/usr/local/java" >>/etc/profile
+                echo "PATH=\$JAVA_HOME/bin:\$PATH" >>/etc/profile
+                echo "export JAVA_HOME PATH" >>/etc/profile
+
+                source /etc/profile
+
+        else
                 echo "jdk already installed"
                 exit 0
         fi
+
+}
+
+install_nexus(){
 
 	#install nexus
 
@@ -81,6 +84,8 @@ EOF
 
 
 }
+
+install_jdk
 source /etc/profile
 install_nexus
 
