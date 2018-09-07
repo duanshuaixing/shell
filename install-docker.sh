64eb0baca6819f35347b921aecc7691d5760ece8
duanshuaixing <1935829085@qq.com>
2018-09-07 10:42:52 +0800

    install docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 install_docker(){
+
+	docker version &>/dev/null
+
+        if [ $? -eq 0 ];then
+                echo "docekr already installed"
+                exit 0
+        fi
+
+        if [ $UID -ne 0 ];then
+                echo "Permission denied"
+                exit 1
+        fi
+
 	wget https://github.com/duanshuaixing/shell/blob/master/docker-ce.repo -P /etc/yum.repos.d/
 	yum -y install yum-utils device-mapper-persistent-data lvm2
 	yum makecache fast
