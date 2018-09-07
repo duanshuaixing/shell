7868fa301d0cacd79111ddf96b1230b7bf734d89
duanshuaixing <1935829085@qq.com>
2018-09-07 10:21:47 +0800

    remove directory from remote repository

--- clean-history.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-echo >/var/log/wtmp
-echo >/var/log/btmp
-echo >/var/log/secure
-echo >/var/log/messages
-echo >/var/log/yum.log
-echo >/var/log/secure
-echo >/root/.bash_history
-history -c
